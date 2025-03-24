fa8a5bc06e49290e04fc148efd5d214070256e37
Stealthmefie <stealthmefi@gmail.com>
2025-03-24 21:11:57 +0500

    HP bar and Stamina bar + woundx3

--- scripts/PlayerScripts/PlayerData.cs
+++ scripts/PlayerScripts/PlayerData.cs
@@ -4,6 +4,13 @@ using System;
 public partial class PlayerData : Node
 {
 	public static int PlayerHealth = 100;
+
+	public static int PlayerMaxHealth = 100;
+
+	public static int PlayerStamina = 100;
+
+	public static int PlayerMaxStamina = 100;
+
 	public override void _Ready()
 	{
 	}
