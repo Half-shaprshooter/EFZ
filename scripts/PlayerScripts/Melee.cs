b62cb6a9905d81e30b686c61381cd880e1e1190a
1Lence <kirillpetrov173@gmail.com>
2025-03-28 17:45:45 +0500

    Переделал свет в домах, добавил проверку на оружие в руках

---
+++ scripts/PlayerScripts/Melee.cs
@@ -0,0 +1,38 @@
+namespace EscapeFromZone.scripts.PlayerScripts;
+
+public partial class Melee : RigidBody2D
+{
+	[Export] public float damage = 1000f;
+	[Export] public float speed = 1000f;
+
+	//Не доделано
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Transform.X * speed * (float)delta;
+		Vector2 newPosition = GlobalPosition + velocity;
+
+		var spaceState = GetWorld2D().DirectSpaceState;
+		var query = PhysicsRayQueryParameters2D.Create(GlobalPosition, newPosition);
+		var result = spaceState.IntersectRay(query);
+
+		if (result != null && result.ContainsKey("collider"))
+		{
+			var collider = (Node)result["collider"];
+			if (collider != null && collider.IsInGroup("Alive"))
+			{
+				var health = collider.GetNodeOrNull<Health>("Health");
+				var host = collider.GetNodeOrNull<HostImpl>("HostImpl");
+				GD.Print(collider + " is before " + host._host);
+				host._host = Host.Enemy;
+				GD.Print(collider + " is after " + host._host);
+				health?.Damage(damage);
+			}
+			GlobalPosition = (Vector2)result["position"];
+			QueueFree();
+		}
+		else
+		{
+			GlobalPosition = newPosition;
+		}
+	}
+}
--- scripts/gdScripts/roof.gd
+++ scripts/gdScripts/roof.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
 @onready var roof := $RoofSpriteGroup
+@onready var lights := $LightsGroup
+
 
 func _on_area_2d_body_entered(body) -> void:
+		lights.show();
 		roof.hide();
 
 func _on_area_2d_body_exited(body) -> void:
+		lights.hide();
 		roof.show()
